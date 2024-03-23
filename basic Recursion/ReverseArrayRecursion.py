aed740d45d87581b0ab31deb7141e58128a57dac
rohithredddy <141226920+rohithredddy@users.noreply.github.com>
2024-03-23 19:33:49 +0530

    22/03/2024

--- basic Recursion/ReverseArrayRecursion.py
+++ basic Recursion/ReverseArrayRecursion.py
@@ -1,3 +1,4 @@
+#two pointer approach
 a=[41,52,86,96,85,45,-85,45,-54]
 def RevereseArrayRecursion(l,r):
     if l>=r:
@@ -5,4 +6,15 @@ def RevereseArrayRecursion(l,r):
     a[l],a[r]=a[r],a[l]
     RevereseArrayRecursion(l+1,r-1)
 RevereseArrayRecursion(0,len(a)-1)
+print(a)
+
+# one pointer approach 
+
+a=[1,2,3,4,5]
+def RevereseArrayRecursion1(i):
+    if i>=len(a)//2:
+        return
+    a[i],a[len(a)-i-1]=a[len(a)-i-1],a[i]
+    RevereseArrayRecursion1(i+1)
+RevereseArrayRecursion1(0)
 print(a)
