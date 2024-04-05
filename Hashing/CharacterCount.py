3ac3dfcfea6308f47f8c533da12ba3b4403d34ff
rohithredddy <141226920+rohithredddy@users.noreply.github.com>
2024-04-05 22:47:38 +0530

    05/04/2024

---
+++ Hashing/CharacterCount.py
@@ -0,0 +1,9 @@
+def charctercount(x,a):
+    cnt=0
+    for i in a:
+        if i==x:
+            cnt+=1
+    return cnt
+print(charctercount("a","supriyasrujanarohithsrikanth")) 
+
+# counting a character in string without using hashing
