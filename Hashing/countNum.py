9ad1c3748183f13fc6a2e8896932f21273e73b4b
rohithredddy <141226920+rohithredddy@users.noreply.github.com>
2024-04-05 22:42:28 +0530

    05/04/2024

---
+++ Hashing/countNum.py
@@ -0,0 +1,13 @@
+
+def countarray(number,arr):
+    cnt=0
+    for i in arr:
+        if i==number:
+            cnt+=1
+    return cnt
+arr=[10,20,30,5,4,7,5,8,6,5,9,8]
+
+
+# counting a number that appears how many times in array
+
+
